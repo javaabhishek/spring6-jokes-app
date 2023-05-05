87b71d722ab3407e573e2867e28070fa00e8ce94
javaabhishek <java.abhishek.patil@gmail.com>
2023-05-05 10:30:07 +0530

    Create spring boot project #1

---
+++ src/main/java/com/asoft/spring6jokesapp/Spring6JokesAppApplication.java
@@ -0,0 +1,13 @@
+package com.asoft.spring6jokesapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring6JokesAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring6JokesAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/asoft/spring6jokesapp/Spring6JokesAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.asoft.spring6jokesapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring6JokesAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
